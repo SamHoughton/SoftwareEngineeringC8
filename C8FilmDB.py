3f0e7bf6e20f45a9e18fcdfca8a1e3725ecdece5
SamHoughton <43782912+SamHoughton@users.noreply.github.com>
2018-11-01 14:02:10 +0000

    Add files via upload

--- C8FilmDB.py
+++ C8FilmDB.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+import requests
+response = requests.get("http://www.omdbapi.com/?i=tt3896198&apikey=3f3265e5")
+print(response.content)
 
