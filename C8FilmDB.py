d4df1192adc15bf0f7b11e9a6893adbcdecb0f21
Caleb Penning <penningc@me.com>
2018-11-01 14:27:40 +0000

    3rd Commit
    Introductions to Interface

--- C8FilmDB.py
+++ C8FilmDB.py
@@ -2,3 +2,13 @@ import requests
 response = requests.get("http://www.omdbapi.com/?i=tt3896198&apikey=3f3265e5")
 print(response.content)
 
+import tkinter
+top = tkinter.Tk()
+
+w = Button (master, )
+
+top.mainloop()
+
+
+
+
