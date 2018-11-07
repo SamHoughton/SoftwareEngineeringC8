52b46a719f4724c66741676d2992338cfa62e1fb
Caleb Penning <penningc@me.com>
2018-11-07 11:56:52 +0000

    Alpha Search Interface Commit 1

--- C8FilmDB.py
+++ C8FilmDB.py
@@ -1,14 +1,42 @@
 import requests
-response = requests.get("http://www.omdbapi.com/?i=tt3896198&apikey=3f3265e5")
-print(response.content)
+#response = requests.get("http://www.omdbapi.com/?i=tt3896198&apikey=3f3265e5")
+#print(response.content)
 
-import tkinter
-top = tkinter.Tk()
+import tkinter as tk
 
-w = Button (master, )
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
-top.mainloop()
+    
 
+    def create_widgets(self):
+        
+        self.Search = tk.Entry()
+        self.Search.pack()
+        self.SearchContents = StringVar()
+        # set it to some value
+        self.SearchContents.set("Arrival")
+        # tell the entry widget to watch this variable
+        self.Search["textvariable"] = self.SearchContents
+        self.Search.bind('<Key-Return>', self.MovieSearch)
+        
+        self.MovieBtn = tk.Button(self)
+        self.MovieBtn["text"] = "Find a\n Random Movie"
+        self.MovieBtn["command"] = self.MovieSearch
+        self.MovieBtn.pack(side="top")
 
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
 
+    def MovieSearch(self):
+        response = requests.get(self.SearchContents)
+        print(response.content)
 
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
