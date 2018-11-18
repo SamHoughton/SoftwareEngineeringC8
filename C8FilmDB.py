23a4881feb836a5fdf76587c30bf801273be125b
Caleb Penning <penningc@me.com>
2018-11-18 18:04:10 +0000

    Commit 7
    Added:
    Comments and a better Structure
    Imports OMDb API
    More Readable GUI
    Labels for Movie Info

--- C8FilmDB.py
+++ C8FilmDB.py
@@ -1,57 +1,134 @@
+#============Importing the APIs==================#
 import requests
 response = requests.get("http://www.omdbapi.com/?t=blade&apikey=3f3265e5")
-#print(response.content)
 
-#import omdb
-
-#res = omdb.request(t='Lion King', y=1994, r='json')
-#xml_content = res.content
-#print(omdb.search(t='Lion King') )
-
-
+import omdb
+omdb.set_default('apikey', '3f3265e5')
+res = omdb.request(t='Lion King', y=1994, r='xml')
+xml_content = res.content
 
 import tkinter as tk
 
+#------------Application Class-------------------#   
+
 class Application(tk.Frame):
+    
+#------------Initialisation Function-------------#   
+    
     def __init__(self, master=None):
-        super().__init__(master)
+        
+#============Setting Up Everything===============#
+        
+        super().__init__(master, width=500)
         self.master = master
         self.pack()
         self.create_widgets()
-        tk.geometry("500x500")
+        
+#------------Create Widgets Function-------------#   
         
     def create_widgets(self):
         
+#============Labelling The Window================#   
+        
         self.winfo_toplevel().title("Movie Information Client")
+ 
+#============Frame Declarations==================#   
         
-        self.Search = tk.Entry()
-        self.Search.pack()
+        self.SearchFrame = tk.Frame()
+        self.SearchFrame.pack(side = tk.TOP)
+        
+        self.InfoFrame = tk.Frame()
+        self.InfoFrame.pack()
+        
+        TitleFrame = tk.Frame(self.InfoFrame)
+        TitleFrame.pack()
+        
+        YearFrame = tk.Frame(self.InfoFrame)
+        YearFrame.pack()
+        
+        DirectorFrame = tk.Frame(self.InfoFrame)
+        DirectorFrame.pack()
+        
+        GenreFrame = tk.Frame(self.InfoFrame)
+        GenreFrame.pack()
+        
+        self.QuitFrame = tk.Frame()
+        self.QuitFrame.pack(side = tk.BOTTOM)
+
+#============Default Declarations===============#   
+        
+        self.LabelDefault = "--"
+        
+#============Search Widget======================#   
+        
+        self.Search = tk.Entry(self.SearchFrame)
+        self.Search.pack(side = tk.RIGHT, fill=tk.X)
         self.SearchContents = tk.StringVar()
-        
-        # set it to some value
-        self.SearchContents.set("Arrival")
-        
+        self.SearchContents.set("Arrival")         
         # tell the entry widget to watch this variable
         self.Search["textvariable"] = self.SearchContents
         self.Search.bind('<Key-Return>', self.MovieSearch)
         
-        self.MovieBtn = tk.Button(self)
-        self.MovieBtn["text"] = "Find a\n Random Movie"
+        self.MovieBtn = tk.Button(self.SearchFrame, padx=3, pady=3)
+        self.MovieBtn["text"] = "Find a Random Movie"
         self.MovieBtn["command"] = self.MovieSearch
-        self.MovieBtn.pack(side="top")
+        self.MovieBtn.pack(side = tk.LEFT)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
+#============Labels for Movie Info==============#   
+        
+        self.title = tk.Label(TitleFrame, padx=3, pady=3)
+        self.title.pack(side = tk.LEFT)
+        self.title["text"] = "Title:"
+        
+        self.MovieTitle = tk.Label(TitleFrame, padx=3, pady=3)
+        self.MovieTitle.pack()
+        self.MovieTitle["textvariable"] = self.LabelDefault
+        
+        self.year = tk.Label(YearFrame, padx=3, pady=3)
+        self.year.pack(side = tk.LEFT)
+        self.year["text"] = "Year:"
+        
+        self.MovieYear = tk.Label(YearFrame, padx=3, pady=3)
+        self.MovieYear.pack()
+        
+        self.MovieYear["textvariable"] = self.LabelDefault
+        
+        self.director = tk.Label(DirectorFrame, padx=3, pady=3)
+        self.director.pack(side = tk.LEFT)
+        self.director["text"] = "Director:"
+        
+        self.MovieDirector = tk.Label(DirectorFrame, padx=3, pady=3)
+        self.MovieDirector.pack()
+        
+        self.MovieDirector["textvariable"] = self.LabelDefault
+        
+        self.genre = tk.Label(GenreFrame, padx=3, pady=3)
+        self.genre.pack(side = tk.LEFT)
+        self.genre["text"] = "Genre:"
+        
+        self.MovieGenre = tk.Label(GenreFrame, padx=3, pady=3)
+        self.MovieGenre.pack()
+        
+        self.MovieGenre["textvariable"] = self.LabelDefault
+        
+#============Quit Button=========================#   
+        
+        self.quit = tk.Button(self.QuitFrame, text="QUIT", fg="red",
+                              command=self.master.destroy, padx=3, pady=3)
+        self.quit.pack()
+ 
+#------------Movie Search Function---------------#
+    
     def MovieSearch(self):
-        print(self.SearchContents.get())
-        response = requests.get("http://www.omdbapi.com/?t=%s&apikey=3f3265e5" % (self.SearchContents.get()))
-        print(response.content)
-        print("")
+        
+        #print(self.SearchContents.get())
+        #response = requests.get("http://www.omdbapi.com/?t=%s&apikey=3f3265e5" % (self.SearchContents.get()))
+        #print(response.content)
+        
+        print(omdb.get(title=self.SearchContents.get()))
+        
         
         #self.TitleLabel
-        
 
 root = tk.Tk()
 app = Application(master=root)
