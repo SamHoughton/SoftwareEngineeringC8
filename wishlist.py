d95ecca382d316bb0dd2340948bff3ffb2e19d6c
Caleb Penning <penningc@me.com>
2018-11-20 13:26:45 +0000

    Commit 8
    Added:
    Revamped Interface:
    Improving Buttons
    Edited Frame Box
    Fixed Window Size
    Can Remove Item

--- wishlist.py
+++ wishlist.py
@@ -19,6 +19,8 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         master = master
+        master.title("Movie Information Client - Wishlist")
+        master.geometry("600x400+300+200")
         self.pack()
         self.create_widgets()
         master.configure(background="#a1dbcd")
@@ -30,10 +32,10 @@ class Application(tk.Frame):
         #winfo_toplevel().title("Movie Information Client - Wishlist)
         
         title_frame = tk.Frame(root, bg="#a1dbcd")
-        title_frame.pack(side="top")
+        title_frame.pack(side="top", fill=tk.X)
 
-        home = tk.Button(title_frame, text="Home", bg="#a1dbcd") # command=self.master.destroy) #quit application button
-        home.pack(side="left", padx=50, pady=10) #placement of button
+        home = tk.Button(title_frame, text="Home",fg="white", bd=0, bg="#179184",width=10, height=2) # command=self.master.destroy) #quit application button
+        home.pack(side="left", padx=80, pady=10) #placement of button
         
         wishlist = tk.Label(title_frame, bg="#a1dbcd") #creates label
         wishlist["text"] = "WISHLIST" #text on label
@@ -41,42 +43,61 @@ class Application(tk.Frame):
         wishlist.configure(font="titleFont")
         
         search_function = tk.Entry(title_frame, bg="white")
-        search_function.pack(side="left", padx=10, pady=10)
+        search_function.pack(side="right", padx=50, pady=10)
+        
+        
+   #     self.SearchContents = tk.StringVar()
+   #     self.SearchContents.set("Arrival")         
+   #     # tell the entry widget to watch this variable
+   #     search_function["textvariable"] = self.SearchContents
+   #     search_function.bind('<Key-Return>', self.MovieSearch)
+   #     
+   #     MovieBtn = tk.Button(self.SearchFrame, padx=3, pady=3)
+   #     MovieBtn["text"] = "Find Movie"
+   #     MovieBtn["command"] = self.MovieSearch
+   #     MovieBtn.pack(side = tk.LEFT)
+        
         
         main_frame = tk.Frame(root, bg="#a1dbcd")
-        main_frame.pack(side="bottom")
+        main_frame.pack(side="top", fill=tk.BOTH)
+        
+        bottom_frame = tk.Frame(main_frame, bg="#a1dbcd")
+        bottom_frame.pack(side="bottom", fill=tk.X)
         
         scrollbar = tk.Scrollbar(main_frame)
         scrollbar.pack(side="right", fill=tk.Y)
         
-        quit = tk.Button(main_frame, text="Close Application", fg="red", bg="#a1dbcd", command=self.master.destroy) #quit application button
-        quit.pack(side="bottom") #placement of button
+        film_frame = tk.LabelFrame(main_frame, width=500, height=150, bg="white")
+        film_frame.pack(side="top")
         
-        film_frame = tk.LabelFrame(main_frame, width=500, height=150, bg="#a1dbcd", command=self.master.destroy)
-        film_frame.pack(side="bottom")
-        
-        info_frame = tk.Frame(film_frame, bg="#a1dbcd")
+        info_frame = tk.Frame(film_frame, bg="white")
         info_frame.pack(side="top", fill=tk.X)
         
-        title = tk.Label(info_frame, bg="#a1dbcd")
+        title = tk.Label(info_frame, bg="white")
         title["text"] = "Film Title:"
         title.pack(side="top", padx=10, pady=10)
         
-        date = tk.Label(info_frame, bg="#a1dbcd")
+        date = tk.Label(info_frame, bg="white")
         date["text"] = "Date Added to Wishlist:"
         date.pack(side="top", padx=10, pady=10)
                       
                               
-        mini_frame = tk.Frame(film_frame, bg="#a1dbcd")
+        mini_frame = tk.Frame(film_frame, bg="white")
         mini_frame.pack(side="bottom", fill=tk.X)
                               
-        remove = tk.Button(mini_frame, text="Remove Film", fg="red", bg="#a1dbcd") #, command=self.display_film) #creates button
+        remove = tk.Button(mini_frame, text="Remove Film", fg="white",bd=0, bg="#179184",width=20, height=2, command=film_frame.destroy) #, command=self.display_film) #creates button
         #remove["command"] = self.remove_film #create command when button is pressed
-        remove.pack(side="right", padx=50)
+        remove.pack(side="right", padx=50, pady=10)
         
-        film = tk.Button(mini_frame, text="Show Film Info", bg="#a1dbcd") #, command=self.display_film) #creates button
+        film = tk.Button(mini_frame, text="Show Film Info",fg="white", bd=0, bg="#179184",width=20, height=2) #, command=self.display_film) #creates button
         film["command"] = self.display_film #create command when button is pressed
-        film.pack(side="right", padx=50) #placement of button
+        film.pack(side="right", padx=50, pady=10) #placement of button
+        
+        
+        quit = tk.Button(bottom_frame, text="Close Application", fg="white",bd=0, bg="#af1700", width=20, height=2, command=self.master.destroy) #quit application button
+        quit.pack(side="bottom", pady=10) #placement of button
+        
+        
 
 
         #text = tk.Entry(root, width=35, bg="#a1dbcd")
@@ -106,6 +127,36 @@ class Application(tk.Frame):
         
         tk.Label(film_description, text='Film Title:\nDate of Release:\nDirector:\nCast:\nDate Added to Wishlist:\nFilm Description:', bg="#a1dbcd").pack(side="left", padx=30, pady=30)
     
+    
+    #------------Movie Search Function---------------#
+    
+  #  def MovieSearch(self):
+        
+        #print(self.SearchContents.get())
+        #response = requests.get("http://www.omdbapi.com/?t=%s&apikey=3f3265e5" % (self.SearchContents.get()))
+        #print(response.content)
+        #print(omdb.get(title=self.SearchContents.get()))
+        
+  #      response = requests.get("http://www.omdbapi.com/?t=%s&apikey=3f3265e5" % (self.SearchContents.get()))
+  #      movie_dictionary_info = json.loads(response.text)
+  #      print(movie_dictionary_info)
+  #      
+  #      self.TitleX = tk.StringVar()
+  #      self.TitleX.set(movie_dictionary_info.get("Title"))
+  #      
+  #      self.YearX = tk.StringVar()
+  #      self.YearX.set(movie_dictionary_info.get("Year"))
+  #      
+  #      self.DirectorX = tk.StringVar()
+  #      self.DirectorX.set(movie_dictionary_info.get("Director"))
+  #      
+  #      self.GenreX = tk.StringVar()
+  #      self.GenreX.set(movie_dictionary_info.get("Genre"))
+  #      
+  #      self.MovieTitle["textvariable"] = self.TitleX
+  #      self.MovieYear["textvariable"] = self.YearX
+  #      self.MovieDirector["textvariable"] = self.DirectorX
+  #      self.MovieGenre["textvariable"] = self.GenreX
         
    # def text_update(film):
     #    film.text.delete(0, tk.END)
